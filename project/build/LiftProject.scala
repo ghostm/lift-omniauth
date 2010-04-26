0172d2f88350d27b3398c722295938e673589685
David Pollak <feeder.of.the.bears@gmail.com>
2010-04-25 18:56:00 -0700

    Added the stuff necessary for test

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -18,6 +18,8 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "net.liftweb" % "lift-testkit" % liftVersion % "compile->default",
     "net.liftweb" % "lift-wizard" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "junit" % "junit" % "4.5" % "test->default",
+    "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default",
     "com.h2database" % "h2" % "1.2.121"
   ) ++ super.libraryDependencies
 }
