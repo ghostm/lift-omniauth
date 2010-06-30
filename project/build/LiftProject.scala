4042aa306c32480ae09ce6b8a9b96994eaafdc36
David Pollak <feeder.of.the.bears@gmail.com>
2010-06-29 20:14:59 -0700

    Updated to Lift 2.0

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -7,13 +7,11 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   val scalatools_release = "Scala Tools Snapshot" at
   "http://scala-tools.org/repo-releases/"
 
-  val liftVersion = "2.0-RC2"
+  val liftVersion = "2.0"
 
   override def libraryDependencies = Set(
     "net.liftweb" % "lift-webkit" % liftVersion % "compile->default",
     "net.liftweb" % "lift-mapper" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-testkit" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-wizard" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test->default",
