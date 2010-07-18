77e5043b9be6b170919b3ea6b96bc7c650342039
David Pollak <feeder.of.the.bears@gmail.com>
2010-07-18 13:29:14 -0700

    Updated to Lift 2.1-SNAPSHOT

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,14 +1,21 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.0"
+  val liftVersion = "2.1-SNAPSHOT"
+
+  val scalatoolsSnapshot = 
+    "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
+
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  // override def scanDirectories = Nil
 
   override def libraryDependencies = Set(
-    "net.liftweb" % "lift-webkit" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-mapper" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
     "com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 }
