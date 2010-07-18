c2a6875bc5ae8672d314d7a636512e4406be0bcf
David Pollak <feeder.of.the.bears@gmail.com>
2010-07-18 12:49:41 -0700

    Updated project file

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,12 +1,6 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val mavenLocal = "Local Maven Repository" at
-  "file://"+Path.userHome+"/.m2/repository"
-
-  val scalatools_release = "Scala Tools Snapshot" at
-  "http://scala-tools.org/repo-releases/"
-
   val liftVersion = "2.0"
 
   override def libraryDependencies = Set(
@@ -15,6 +9,6 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test->default",
-    "com.h2database" % "h2" % "1.2.121"
+    "com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 }
