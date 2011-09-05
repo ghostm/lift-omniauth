78dd9914d444200dc52667b5b7de4dd0a735d05a
Jonathan Ferguson <jono@spiralarm.com>
2011-09-05 17:01:53 +1000

    Facebook images taken from :  https://github.com/facebook/facebook-ios-sdk
    I believe these should be removed and Facebook javascript should be used to generate the links and images. See http://developers.facebook.com/docs/reference/javascript/.

--- src/main/scala/omniauth/snippet/SignUp.scala
+++ src/main/scala/omniauth/snippet/SignUp.scala
@@ -4,21 +4,25 @@ import net.liftweb.http.LiftRules
 
 object SignUp {
   
-  val path = "/auth/twitter/signin"
+  val twitterPath = "/auth/twitter/signin"
+  val facebookPath = "/auth/facebook/signin"
   
-  val altText = "Sign in with Twitter"
+  val twitterAltText = "Sign in with Twitter"
+  val facebookAltText = "Sign in with facebook"
     
-  private def twitter(img:String) =  <a href={path} ><img src={S.contextPath + "/" + LiftRules.resourceServerPath + "/img/" + img} alt={altText} /></a> 
-    
-  def TwitterLightBackground = twitter("sign-in-with-twitter-l.png")
-  
-  def TwitterDarkBackground = twitter("sign-in-with-twitter-d.png")
-
-  def TwitterTransparentLightIcon = twitter("sign-in-with-twitter-l-sm.png")
-  
-  def TwitterTransparentDarkIcon =twitter("sign-in-with-twitter-d-sm.png") 
-
+  private def twitter(img:String) =  <a href={twitterPath} ><img src={S.contextPath + "/" + LiftRules.resourceServerPath + "/img/" + img + ".png"} alt={twitterAltText} /></a> 
+  private def facebook(img:String) = <a href={facebookPath} ><img src={S.contextPath + "/" + LiftRules.resourceServerPath + "/img/" + img + ".png"} alt={facebookAltText} /></a>
   
   
-
+  def TwitterLightBackground = twitter("sign-in-with-twitter-l")
+  def TwitterDarkBackground = twitter("sign-in-with-twitter-d")
+  def TwitterTransparentLightIcon = twitter("sign-in-with-twitter-l-sm")
+  def TwitterTransparentDarkIcon =twitter("sign-in-with-twitter-d-sm") 
+  
+  def FacebookNormal = twitter("LoginWithFacebookNormal")
+  def FacebookNormalDark = twitter("LoginWithFacebookNormalDark")
+  def FacebookLarge = twitter("LoginWithFacebookLarge")
+  def FacebookLargeDark = twitter("LoginWithFacebookLargeDark")
+   
+  
 }
